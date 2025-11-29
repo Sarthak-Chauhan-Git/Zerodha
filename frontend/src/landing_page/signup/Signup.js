e6dbd50b7fffeb356eae28a057c9d36b62058ca5
Sarthak-Chauhan-Git <sarthakchauhan.official@gmail.com>
2025-11-29 09:41:40 +0530

    Convert frontend from submodule to normal folder

---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <main className="container py-5">
+      <h1>Signup</h1>
+      <p>
+        This is the signup page. Replace this placeholder with your signup form.
+      </p>
+    </main>
+  );
+}
+
+export default Signup;
